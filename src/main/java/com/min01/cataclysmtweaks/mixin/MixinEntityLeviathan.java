68311ee1278e32caf523f3c6c11416d2ed1564eb
min <goyoumin2@gmail.com>
2023-12-28 10:46:12 +0900

    cataclysm mod update

--- src/main/java/com/min01/cataclysmtweaks/mixin/MixinEntityLeviathan.java
+++ src/main/java/com/min01/cataclysmtweaks/mixin/MixinEntityLeviathan.java
@@ -3,6 +3,7 @@ package com.min01.cataclysmtweaks.mixin;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,18 +22,17 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.players.OldUsersConverter;
-import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MoverType;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.fluids.FluidType;
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 
 @Mixin(The_Leviathan_Entity.class)
 public abstract class MixinEntityLeviathan extends Mob implements ITamableLeviathan
@@ -140,10 +140,11 @@ public abstract class MixinEntityLeviathan extends Mob implements ITamableLeviat
 	private void travel(Vec3 travelVector, CallbackInfo ci)
 	{
 		ci.cancel();
-		if(this.isTame() && this.getOwner() != null)
+		LivingEntity livingentity = (LivingEntity) this.getFirstPassenger();
+		boolean flag = this.isVehicle() && livingentity != null && this.isTame() && livingentity == this.getOwner();	
+		if (this.isVehicle() && livingentity != null)
 		{
-			LivingEntity livingentity = this.getOwner();
-			if (this.isVehicle() && this.getFirstPassenger() != null && livingentity == this.getFirstPassenger())
+			if(this.isTame() && livingentity == this.getOwner())
 			{
 	            this.setYRot(livingentity.getYRot());
 	            this.yRotO = this.getYRot();
@@ -151,84 +152,37 @@ public abstract class MixinEntityLeviathan extends Mob implements ITamableLeviat
 	            this.setRot(this.getYRot(), this.getXRot());
 	            this.yBodyRot = this.getYRot();
 	            this.yHeadRot = this.yBodyRot;
-	            float f = livingentity.xxa * 0.5F;
-	            float f1 = livingentity.zza;
-
-	            if (livingentity.hasImpulse) 
-	            {
-	            	float jumpFactor = ((EntityInvoker)livingentity).invoke_getBlockJumpFactor();
-	            	double d1 = jumpFactor * (double)this.getBlockJumpFactor();
-	            	Vec3 vec3 = this.getDeltaMovement();
-	            	this.setDeltaMovement(vec3.x, d1, vec3.z);
-	            	this.hasImpulse = true;
-	            	net.minecraftforge.common.ForgeHooks.onLivingJump(this);
-	            	if (f1 > 0.0F) 
-	            	{
-	            		float f2 = Mth.sin(this.getYRot() * ((float)Math.PI / 180F));
-	            		float f3 = Mth.cos(this.getYRot() * ((float)Math.PI / 180F));
-	            		this.setDeltaMovement(this.getDeltaMovement().add((double)(-0.4F * f2 * jumpFactor), 0.0D, (double)(0.4F * f3 * jumpFactor)));
-	            	}
-	            }
-
-	            this.flyingSpeed = this.getSpeed() * 0.1F;
-	            if (this.isControlledByLocalInstance())
-	            {
-	            	this.setSpeed((float)this.getAttributeValue(Attributes.MOVEMENT_SPEED));
-	            	super.travel(new Vec3((double)f, travelVector.y, (double)f1));
-	            }
-	            else if (livingentity instanceof Player)
-	            {
-	            	this.setDeltaMovement(Vec3.ZERO);
-	            }
-
-	            this.calculateEntityAnimation(this, false);
 			}
 		}
+		Vec3 vec = flag ? new Vec3(livingentity.xxa, (boolean) ObfuscationReflectionHelper.getPrivateValue(LivingEntity.class, livingentity, "f_20899_") ? this.getBlockJumpFactor() + 2.7 : travelVector.y, livingentity.zza) : travelVector;
+		if (this.isEffectiveAi() && this.isInWater()) 
+		{
+			super.travel(vec);
+			this.moveRelative(this.getSpeed(), vec);
+			this.move(MoverType.SELF, this.getDeltaMovement());
+			this.setDeltaMovement(this.getDeltaMovement().scale(0.9D));
+			if (this.getTarget() == null && The_Leviathan_Entity.class.cast(this).getAnimation() == The_Leviathan_Entity.NO_ANIMATION)
+			{
+				this.setDeltaMovement(this.getDeltaMovement().add(0.0D, -0.005D, 0.0D));
+			}
+		} 
 		else
 		{
-			if (this.isEffectiveAi() && this.isInWater())
-			{
-				this.moveRelative(this.getSpeed(), travelVector);
-				this.move(MoverType.SELF, this.getDeltaMovement());
-	            this.setDeltaMovement(this.getDeltaMovement().scale(0.9D));
-	            if (this.getTarget() == null && The_Leviathan_Entity.class.cast(this).getAnimation() == The_Leviathan_Entity.NO_ANIMATION)
-	            {
-	            	this.setDeltaMovement(this.getDeltaMovement().add(0.0D, -0.005D, 0.0D));
-	            }
-			} 
-			else 
-			{
-				super.travel(travelVector);
-			}
+			super.travel(vec);
 		}
 	}
 	
-	@Inject(at = @At("HEAD"), method = "positionRider", cancellable = true)
-	public void positionRider(Entity passenger, CallbackInfo ci) 
+	@Inject(at = @At("HEAD"), method = "HoldAttack", cancellable = true, remap = false)
+	private void HoldAttack(CallbackInfo ci) 
 	{
-		ci.cancel();
-		super.positionRider(passenger);
-		if(passenger != this.getOwner())
+		LivingEntity lifted = getHeldEntity();
+		if (lifted != null) 
 		{
-			if (this.hasPassenger(passenger))
+			if(lifted == this.getOwner())
 			{
-				if (The_Leviathan_Entity.class.cast(this).getAnimation() == The_Leviathan_Entity.LEVIATHAN_TENTACLE_HOLD_BLAST) 
-				{
-					if (The_Leviathan_Entity.class.cast(this).getAnimationTick() == 169)
-					{
-						passenger.stopRiding(); 
-					}
-					this.setXRot(this.xRotO);
-					this.yBodyRot = this.getYRot();
-					this.yHeadRot = this.getYRot();
-				} 
-				float f17 = this.getYRot() * 3.1415927F / 180.0F;
-				float pitch = this.getXRot() * 3.1415927F / 180.0F;
-				float f3 = Mth.sin(f17) * (1.0F - Math.abs(this.getXRot() / 90.0F));
-		        float f18 = Mth.cos(f17) * (1.0F - Math.abs(this.getXRot() / 90.0F));
-		        passenger.setPos(this.getX() + (f3 * -8.25F), this.getY() + (-pitch * 6.0F), this.getZ() + (-f18 * -8.25F));
+				ci.cancel();
 			}
-		}
+		} 
 	}
 
 	@Inject(at = @At("HEAD"), method = "hurt", cancellable = true)
@@ -260,6 +214,19 @@ public abstract class MixinEntityLeviathan extends Mob implements ITamableLeviat
 		{
 			ci.cancel();
 		}
+	}
+	
+	@Shadow
+	public void setheldEntity(int p_175463_1_)
+	{
+		
+	}
+	
+	@Nullable
+	@Shadow
+	public LivingEntity getHeldEntity()
+	{
+		throw new IllegalStateException();
 	}
 
 	@Override
